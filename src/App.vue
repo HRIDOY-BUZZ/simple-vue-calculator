f35a95da3d02ef59fd2d2e70c871c89ff7807285
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2023-03-18 08:55:02 +0600

    repo created

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Header from './components/Header.vue'
+import Calculator from './components/Calculator.vue'
+</script>
+
+<template>
+  <div>
+
+  </div>
+  <Header msg="Simple Vue Calculator" />
+  <Calculator/>
+</template>
+
+<style scoped>
+
+</style>
